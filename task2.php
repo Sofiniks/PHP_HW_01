7babaa7e304a00e44e892d334dc44ce4d7ee6b24
Sofiniks98 <sofija.nikitina9@gmail.com>
2022-01-13 23:42:15 +0200

    first commit

--- task2.php
+++ task2.php
@@ -6,5 +6,5 @@ const PENCILS_PICS = 40;
 
 echo "На школьной выставке " . ALL_PICS . " рисунков. " . FELT_TIP_PAN_PICS . " из них выполнены фломастерами, " . PENCILS_PICS . " карандашами, а остальные — красками. Сколько рисунков, выполненные красками, на школьной выставке?" . "<br>";
 
-$PAINT_PICS = ALL_PICS - FELT_TIP_PAN_PICS - PENCILS_PICS;
-echo "Ответ: $PAINT_PICS";
+$paintPics = ALL_PICS - FELT_TIP_PAN_PICS - PENCILS_PICS;
+echo "Ответ: $paintPics";
